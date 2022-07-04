fb920ca1cda9568124c4419816cae90deca54b2e
PauloMtz <pramosmtz@gmail.com>
2022-07-04 07:28:50 -0300

    Criando AuthContext

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,4 +1,7 @@
+import { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { AuthContext } from '../contexts/auth';
+// importação entre chaves porque não é export default lá na origem
 
 export default function RouteWrapper({
   component: Component,
@@ -6,8 +9,8 @@ export default function RouteWrapper({
   ...rest
 }){
 
-  const loading = false;
-  const signed = false;
+  // agora, signed e loading vêm de AuthContext
+  const { signed, loading } = useContext(AuthContext);
 
   if(loading){
     return(
