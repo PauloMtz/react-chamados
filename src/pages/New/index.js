ff325ece4ec63e6a3f5de91972c1daf2ace92999
PauloMtz <pramosmtz@gmail.com>
2022-07-11 13:13:50 -0300

    Criando a página de Novo Chamado #02

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,15 +1,35 @@
+import { useState } from 'react';
+
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 
 import './style.css';
 import { FiPlusCircle } from 'react-icons/fi'
 
-export default function New() {
+export default function New(){
+  
+  const [assunto, setAssunto] = useState('Suporte');
+  const [status, setStatus] = useState('Aberto');
+  const [complemento, setComplemento] = useState('');
 
-  function handleRegister(e) {
+  function handleRegister(e){
     e.preventDefault();
 
     alert('TESTE')
+  }
+
+
+  // quando muda o assunto
+  function handleChangeSelect(e){
+    setAssunto(e.target.value);
+    console.log(e.target.value);
+  }
+
+
+  // quando muda o status
+  function handleOptionChange(e){
+    setStatus(e.target.value);
+    console.log(e.target.value);
   }
 
   return(
@@ -33,7 +53,7 @@ export default function New() {
             </select>
 
             <label>Assunto</label>
-            <select>
+            <select value={assunto} onChange={handleChangeSelect}>
               <option value="Suporte">Suporte</option>
               <option value="Visita Tecnica">Visita Tecnica</option>
               <option value="Financeiro">Financeiro</option>
@@ -45,6 +65,8 @@ export default function New() {
               type="radio"
               name="radio"
               value="Aberto"
+              onChange={handleOptionChange}
+              checked={ status === 'Aberto' }
               />
               <span>Em Aberto</span>
 
@@ -52,6 +74,8 @@ export default function New() {
               type="radio"
               name="radio"
               value="Progresso"
+              onChange={handleOptionChange}
+              checked={ status === 'Progresso' }
               />
               <span>Progresso</span>
 
@@ -59,6 +83,8 @@ export default function New() {
               type="radio"
               name="radio"
               value="Atendido"
+              onChange={handleOptionChange}
+              checked={ status === 'Atendido' }
               />
               <span>Atendido</span>
             </div>
@@ -67,6 +93,8 @@ export default function New() {
             <textarea
               type="text"
               placeholder="Descreva seu problema (opcional)."
+              value={complemento}
+              onChange={ (e) => setComplemento(e.target.value) }
             />
             
             <button type="submit">Registrar</button>
